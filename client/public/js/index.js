bafc339bcac1cd1a68a107050e7178b6a34f4fbf
Oleg Stotsky <ostotsky@gmail.com>
2017-12-21 15:40:40 +0300

    add message broadcast mockup

---
+++ client/public/js/index.js
@@ -0,0 +1,14 @@
+const socket = io();
+socket.on('connect', function() {
+  console.log('Connected to server');
+});
+socket.on('disconnect', function() {
+  console.log('Disconnected from server');
+});
+socket.on('newMessage', function(data) {
+  console.log(data);
+});
+socket.emit('createMessage', {
+  from: 'Oleg',
+  text: 'Some text'
+}); 
--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const socketIO = require('socket.io');
 const path = require('path');
+const http = require('http');
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 const publicPath = path.join(__dirname, '..', 'client', 'public');
 const port = process.env.PORT || 3000;
 app.use('/', express.static(publicPath));
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('New user connected');
+
+  socket.on('createMessage', function(message) {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  })
+  socket.on('disconnect', () => {
+    console.log('Disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log('listening on port', port);
 });
